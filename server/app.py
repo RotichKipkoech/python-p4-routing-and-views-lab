a2100be33ee387a1f26dc6c59491ba3081825c2e
RotichKipkoech <kipkoechrottich@gmail.com>
2023-09-19 09:56:41 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/<string:operation>')
+def print(operation):
+    return f'<h1>Welcome to my app! {operation}</h1>'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
